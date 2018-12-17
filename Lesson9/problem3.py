82659f864b30082cdd3564a6b8e2ef2c2cf7e159
pleszkoactvf <43756310+pleszkoactvf@users.noreply.github.com>
2018-12-17 12:27:46 -0500

    Add files via upload

---
+++ Lesson9/problem3.py
@@ -0,0 +1,9 @@
+print('-'*65)
+print('Alien Greater Bot:')
+print()
+planet = input('What planet are you from? ')
+if planet == 'Mars':
+	print('Welcome to Earth, please do not kill us!')
+
+else:
+	print('Dammmmmnnnn, your face is so ugly I thought you were an alien. Anyway...have a good day.')
